9a4512aea62381b3d88fcb1201e24399b5bfbb21
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-12 10:38:45 +0100

    params for group for discrete log added

--- chaum_pedersen/src/protocol/constants.rs
+++ chaum_pedersen/src/protocol/constants.rs
@@ -8,6 +8,16 @@ use pasta_curves::pallas::Point as PallasPoint;
 use pasta_curves::vesta::Point as VestaPoint;
 use std::str::FromStr;
 
+lazy_static! {
+    pub static ref DLOG_GROUP_PARAMS: GroupParams<BigUint> = {
+        GroupParams {
+            p: BigUint::from_str("42765216643065397982265462252423826320512529931694366715111734768493812630447").unwrap(),
+            q: BigUint::from_str("21382608321532698991132731126211913160256264965847183357555867384246906315223").unwrap(),
+            g: BigUint::from_str("4").unwrap(),
+            h: BigUint::from_str("9").unwrap(),
+        }
+    };
+}
 
 fn convert(vec: &Vec<u8>) -> Result<&[u8; 32], &'static str> {
     if vec.len() == 32 {
