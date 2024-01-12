d082740a853252cd958b8e3c8161b42d69cc3f89
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-12 11:05:26 +0100

    `InMemoryUserAPI` struct added

---
+++ server/src/apis/user_impl/in_memory.rs
@@ -0,0 +1,13 @@
+use crate::apis::user::UserAPI;
+use crate::apis::user::User;
+use chaum_pedersen::traits::{FromBytes, IntoBytes};
+use std::collections::HashMap;
+use uuid::Uuid;
+
+use crate::apis::user::AuthChallenge;
+
+/// This struct for an in-memory implementation of the `UserAPI` trait using hash maps.
+pub struct InMemoryUserAPI<T, S> {
+    users: HashMap<String, User<T>>,
+    auth_challenges: HashMap<String, AuthChallenge<S>>,
+}
