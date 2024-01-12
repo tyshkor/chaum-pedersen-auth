0ce295e25af4b5d94b8f01fd68631bd774297e96
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-12 11:03:53 +0100

    `UserAPI` trait added

--- server/src/apis/user.rs
+++ server/src/apis/user.rs
@@ -13,3 +13,23 @@ pub struct AuthChallenge<S> {
     pub user: String,
     pub c: S,
 }
+
+/// Trait defining the API for User struct.
+///
+/// This trait abstracts the CRUD (Create, Read, Update, Delete) operations
+/// and authentication challenge related operations for user data.
+pub trait UserAPI<T, S> {
+    fn create(&mut self, user: User<T>);
+
+    fn read(&mut self, username: &str) -> Option<User<T>>;
+
+    fn update(&mut self, name: &String, user: User<T>) -> Option<()>;
+
+    fn delete(&mut self, name: &String) -> Option<User<T>>;
+
+    fn create_auth_challenge(&mut self, user: &String, c: &S) -> String;
+
+    fn delete_auth_challenge(&mut self, id: &String);
+
+    fn get_auth_challenge(&mut self, id: &String) -> Option<AuthChallenge<S>>;
+}
