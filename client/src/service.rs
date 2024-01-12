e035740b99bd9b4435e157f552225800aa73b91d
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-12 11:38:24 +0100

    run_protocol function added

--- client/src/service.rs
+++ client/src/service.rs
@@ -56,3 +56,43 @@ impl AuthClientService {
         Ok(response.into_inner().session_id)
     }
 }
+
+/// Runs the Chaum-Pedersen protocol for client authentication.
+pub async fn run_protocol<T, P, S>(
+    params: &GroupParams<P>, x: &T::Secret, user: &String, client: &mut AuthClientService,
+) -> anyhow::Result<()>
+where
+    T: Protocol<
+        GroupParameters = GroupParams<P>,
+        CommitParameters = (P, P, P, P),
+        Response = S,
+        Challenge = S,
+    >,
+    P: FromBytes<P> + IntoBytes<P> + Random<P>,
+    S: FromBytes<S> + IntoBytes<S> + Random<S>,
+{
+    let ((y1, y2, r1, r2), k) = T::commitment(params, x);
+
+    client
+        .register(user.clone(), P::to(&y1), P::to(&y2))
+        .await?;
+
+    let (c, auth_id) = client
+        .create_authentication_challenge(user.clone(), P::to(&r1), P::to(&r2))
+        .await?;
+
+    let challenge = S::from(&c)?;
+
+    let s = T::challenge_response(&params, &k, &challenge, &x);
+
+    let session_id = client
+        .verify_authentication(auth_id, S::to(&s))
+        .await?;
+
+    println!("Authentication was successful!");
+    println!("Session ID: {}", session_id);
+
+    T::verify(&params, &s, &challenge, &(y1, y2, r1, r2));
+
+    Ok(())
+}
