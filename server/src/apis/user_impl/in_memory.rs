7ebce91ac186cdbc706c0710dcd3cc0771e88d28
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-12 11:06:19 +0100

    `UserAPI` trait implemented for `InMemoryUserAPI`

--- server/src/apis/user_impl/in_memory.rs
+++ server/src/apis/user_impl/in_memory.rs
@@ -20,3 +20,49 @@ impl<T, S> InMemoryUserAPI<T, S> {
         }
     }
 }
+
+impl<T, S> UserAPI<T, S> for InMemoryUserAPI<T, S>
+where
+    T: Send + Sync + 'static + Clone + FromBytes<T> + IntoBytes<T>,
+    S: Send + Sync + 'static + Clone + FromBytes<S> + IntoBytes<S>,
+{
+    fn create(&mut self, user: User<T>) {
+        self.users.insert(user.username.clone(), user);
+    }
+
+    fn read(&mut self, username: &str) -> Option<User<T>> {
+        self.users.get(username).cloned()
+    }
+
+    fn update(&mut self, name: &String, new_user: User<T>) -> Option<()> {
+        if let Some(user) = self.users.get_mut(name) {
+            *user = new_user;
+            Some(())
+        } else {
+            None
+        }
+    }
+
+    fn delete(&mut self, name: &String) -> Option<User<T>> {
+        self.users.remove(name)
+    }
+
+    fn create_auth_challenge(&mut self, user: &String, c: &S) -> String {
+        let uid = Uuid::new_v4().to_string();
+        let auth_challenge = AuthChallenge {
+            id: uid.clone(),
+            user: user.clone(),
+            c: c.clone(),
+        };
+        self.auth_challenges.insert(uid.clone(), auth_challenge);
+        uid
+    }
+
+    fn delete_auth_challenge(&mut self, id: &String) {
+        self.auth_challenges.remove(id);
+    }
+
+    fn get_auth_challenge(&mut self, id: &String) -> Option<AuthChallenge<S>> {
+        self.auth_challenges.get(id).cloned()
+    }
+}
