552de9e28876db48b3b95de56f780bd7601bf174
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-12 11:38:11 +0100

    `AuthClientService` implementation added

--- client/src/service.rs
+++ client/src/service.rs
@@ -20,3 +20,39 @@ pub struct AuthClientService {
     /// The gRPC client for the ZKP authentication service.
     client: AuthClient<Channel>,
 }
+
+impl AuthClientService {
+    pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+    where
+        D: std::convert::TryInto<tonic::transport::Endpoint>,
+        D::Error: Into<StdError>,
+    {
+        let client = AuthClient::connect(dst).await?;
+        Ok(Self { client })
+    }
+
+    pub async fn register(
+        &mut self, user: String, y1: Vec<u8>, y2: Vec<u8>,
+    ) -> Result<(), tonic::Status> {
+        let request = RegisterRequest { user, y1, y2 };
+        self.client.register(request).await?;
+        Ok(())
+    }
+
+    pub async fn create_authentication_challenge(
+        &mut self, user: String, r1: Vec<u8>, r2: Vec<u8>,
+    ) -> Result<(Vec<u8>, String), tonic::Status> {
+        let request = AuthenticationChallengeRequest { user, r1, r2 };
+        let response = self.client.create_authentication_challenge(request).await?;
+        let inner = response.into_inner();
+        Ok((inner.c, inner.auth_id))
+    }
+
+    pub async fn verify_authentication(
+        &mut self, auth_id: String, s: Vec<u8>,
+    ) -> Result<String, tonic::Status> {
+        let request = AuthenticationAnswerRequest { auth_id, s };
+        let response = self.client.verify_authentication(request).await?;
+        Ok(response.into_inner().session_id)
+    }
+}
