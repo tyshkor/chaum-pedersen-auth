a99031dac5ef834a77fbcf909609f6d853538fed
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-12 10:16:32 +0100

    `Protocol` trait definition added

--- chaum_pedersen/src/protocol/mod.rs
+++ chaum_pedersen/src/protocol/mod.rs
@@ -15,3 +15,38 @@ pub struct GroupParams<T> {
     pub q: T,
 }
 
+/// A trait defining the interface for the Chaum-Pedersen zero-knowledge protocol.
+pub trait Protocol {
+    type Secret;
+    type Response;
+    type Challenge;
+    type GroupParameters;
+    type CommitParameters;
+    type CommitmentRandom;
+
+    /// Calculates the commitment in the Chaum-Pedersen protocol.
+    fn commitment(
+        params: &Self::GroupParameters, x: &Self::Secret,
+    ) -> (Self::CommitParameters, Self::CommitmentRandom)
+    where
+        Self: Sized;
+
+    /// Generates a challenge in the Chaum-Pedersen protocol.
+    fn challenge(params: &Self::GroupParameters) -> Self::Challenge
+    where
+        Self: Sized;
+
+    fn challenge_response(
+        params: &Self::GroupParameters, k: &Self::CommitmentRandom, c: &Self::Challenge,
+        x: &Self::Secret,
+    ) -> Self::Response
+    where
+        Self: Sized;
+
+    fn verify(
+        params: &Self::GroupParameters, s: &Self::Response, c: &Self::Challenge,
+        cp: &Self::CommitParameters,
+    ) -> bool
+    where
+        Self: Sized;
+}
