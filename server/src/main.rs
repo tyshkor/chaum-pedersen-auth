177c1979bad4d2a9ddfaa8b3f2da63096ac74831
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-12 11:00:00 +0100

    `main` function implemented

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,81 @@
-fn main() {
-    println!("Hello, world!");
+/// Core services and business logic implementation.
+pub mod service;
+
+/// CRUD APIs to work with storage.
+pub mod apis;
+
+pub mod cli;
+pub mod errors;
+
+use chaum_pedersen::protocol::constants::{DLOG_GROUP_PARAMS, VESTA_GROUP_PARAMS, PALLAS_GROUP_PARAMS};
+use structopt::StructOpt;
+use tonic::transport::Server;
+use chaum_pedersen::protocol::discrete_log::DiscreteLog;
+use chaum_pedersen::protocol::elliptic_curves::pallas::PallasEllipticCurve;
+use chaum_pedersen::protocol::elliptic_curves::vesta::VestaEllipticCurve;
+use chaum_pedersen::enums::{Flavor, EllipticCurve};
+use service::zkp_auth::auth_server::AuthServer;
+use service::AuthService;
+use anyhow::Result;
+
+use crate::cli::Cli;
+use crate::errors::CliError;
+
+/// This starts a server implementing the Chaum-Pedersen protocol through a CLI.
+#[tokio::main]
+async fn main() -> Result<()> {
+    let Cli {
+        host,
+        port,
+        flavor,
+        curve,
+        ..
+    } = Cli::from_args();
+
+    println!(" Starting server ");
+    println!("       host: {}", host);
+    println!("       port: {}", port);
+    println!("       flavor: {}", flavor);
+    if flavor == Flavor::EllipticCurve {
+        println!("       elliptic curve: {}", curve)
+    }
+
+    let addr = format!("{}:{}", host, port)
+        .parse()
+        .map_err(|_| CliError::AddressParsing)?;
+
+    // Initialize and start the server based on flavor.
+    match flavor {
+        Flavor::DiscreteLog => {
+            let params = DLOG_GROUP_PARAMS.to_owned();
+            let auth = AuthService::<DiscreteLog, _, _>::new(params);
+            Server::builder()
+                .add_service(AuthServer::new(auth))
+                .serve(addr)
+                .await?;
+        }
+        Flavor::EllipticCurve => {
+            match curve {
+                EllipticCurve::Pallas => {
+                    let params = PALLAS_GROUP_PARAMS.to_owned();
+                    let auth = AuthService::<PallasEllipticCurve, _, _>::new(params);
+                    Server::builder()
+                        .add_service(AuthServer::new(auth))
+                        .serve(addr)
+                        .await?;
+                }
+
+                EllipticCurve::Vesta => {
+                    let params = VESTA_GROUP_PARAMS.to_owned();
+                    let auth = AuthService::<VestaEllipticCurve, _, _>::new(params);
+                    Server::builder()
+                        .add_service(AuthServer::new(auth))
+                        .serve(addr)
+                        .await?;
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
